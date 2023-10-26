a1297abcc0cc424e9069d11ece139b0bc89eb443
MICAELA RICCARDI <47574710@ort>
2023-10-26 08:30:08 -0300

    Información en tablas lista. (las que no tienen info es porque no tenemos usuarios todavía)

--- Query.sql
+++ Query.sql
@@ -1,5 +1,5 @@
 CREATE PROCEDURE ObtenerPelisPorVer
-	@idUsuario int 
+	@idUsuario INT 
 AS
 BEGIN 
 	SELECT *
@@ -10,8 +10,8 @@ BEGIN
 END
 
 CREATE PROCEDURE ObtenerRating
-	@idUsuario int
-	@idPelicula int
+	@idUsuario INT,
+	@idPelicula INT
 AS
 BEGIN
 	SELECT *
@@ -20,4 +20,52 @@ BEGIN
 		ON Usuario_pelicula.idRating = Rating.idRating
 	WHERE @idUsuario = Usuario_pelicula.idUsuario
 		AND @idPelicula = Usuario_pelicula.idPelicula
+END
+
+CREATE PROCEDURE BuscarPeli
+	@buscar TEXT
+AS
+BEGIN
+	SELECT * 
+	FROM Pelicula
+	WHERE titulo LIKE '%@buscar%'
+END
+
+CREATE PROCEDURE ObtenerUsuario
+	@username text
+AS
+BEGIN
+	SELECT *
+	FROM Usuario
+	WHERE @username LIKE username
+END
+
+CREATE PROCEDURE ObtenerPelisVistas
+	@idUsuario INT
+AS
+BEGIN
+	SELECT *
+	FROM Pelicula
+	INNER JOIN Usuario_pelicula
+		ON pelicula.idPelicula = Usuario_pelicula.idPelicula
+	INNER JOIN Usuario
+		ON Usuario.idUsuario = Usuario_pelicula.idUsuario
+	WHERE Usuario_pelicula.estado = 'True'
+END
+
+CREATE PROCEDURE ObtenerRatings
+	@idUsuario INT
+AS
+BEGIN
+	SELECT *
+	FROM Rating
+	INNER JOIN Usuario_pelicula
+		ON Usuario_pelicula.idRating = Rating.idRating
+	WHERE Usuario_pelicula.idRating IN (SELECT *
+			FROM Pelicula
+			INNER JOIN Usuario_pelicula
+				ON pelicula.idPelicula = Usuario_pelicula.idPelicula
+			INNER JOIN Usuario
+				ON Usuario.idUsuario = Usuario_pelicula.idUsuario
+			WHERE Usuario_pelicula.estado = 'True')
 END
